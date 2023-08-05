0e477bb5bed096b3c6c19ba8650aa92c721af256
Muhammad Ali Yousaf <m.aliyousaf1133@gmail.com>
2023-08-05 13:26:22 -0400

    handle error

--- controllers/authControllers/signUp.js
+++ controllers/authControllers/signUp.js
@@ -1,24 +1,23 @@
 const User = require("../../model/userModel");
-const { sendResponse, errorResponse } = require("../../utils/sendResponse");
+const { sendResponse } = require("../../utils/sendResponse");
 const { validateSignUpInput } = require("../../utils/validationUtils");
+const CatchAsync = require("../../middlewares/CatchAsyncError");
 
-exports.signUp = async (req, res, next) => {
-  try {
-    const validationErrors = validateSignUpInput(req);
-    if (validationErrors.length > 0) {
-      sendResponse(200, validationErrors.join(", "), res);
-    }
-
-    const { accountname } = req.body;
-    const accountExist = await User.findOne({ accountname });
-    if (accountExist) {
-      sendResponse(200, "This account name is already taken", res);
-    }
-
-    await User.create(req.body);
-
-    sendResponse(200, "User registered successfully", res);
-  } catch (error) {
-    errorResponse(error);
+exports.signUp = CatchAsync(async (req, res, next) => {
+  const validationErrors = validateSignUpInput(req);
+  if (validationErrors.length > 0) {
+    sendResponse(200, validationErrors.join(", "), res);
+    return;
   }
-};
+
+  const { accountname } = req.body;
+  const accountExist = await User.findOne({ accountname });
+  if (accountExist) {
+    sendResponse(200, "This account name is already taken", res);
+    return;
+  }
+
+  await User.create(req.body);
+
+  sendResponse(200, "User registered successfully", res);
+});
--- middlewares/CatchAsyncError.js
+++ middlewares/CatchAsyncError.js
@@ -2,6 +2,12 @@ module.exports = (funAsAProp) => async (req, res, next) => {
   try {
     await funAsAProp(req, res, next);
   } catch (error) {
-    next(error);
+    error.statusCode = error.statusCode || 500;
+    error.message = error.message || "Internal Server Error";
+
+    res.status(error.statusCode).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
